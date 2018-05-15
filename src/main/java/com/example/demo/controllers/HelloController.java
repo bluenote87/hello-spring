c74880358d2e695777c00f775c8caf07436505d7
Paul Fannon <lurchworld@gmail.com>
2018-05-15 11:27:40 -0500

    Implemented cookies in the greeting handler.

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.RequestWrapper;
+import javax.servlet.http.Cookie;
 
 @Controller
 public class HelloController {
@@ -46,9 +48,33 @@ public class HelloController {
 
     @RequestMapping(value = "greeting", method=RequestMethod.GET)
     @ResponseBody
-    public static String createMessage() {
+    public static String createMessage(HttpServletResponse response,  HttpServletRequest request) {
+        String nameValue = "";
+        if (request.getCookies() == null) {
+            Cookie enCookie = new Cookie("en","0");
+            response.addCookie(enCookie);
+            Cookie frCookie = new Cookie("fr","0");
+            response.addCookie(frCookie);
+            Cookie esCookie = new Cookie("es","0");
+            response.addCookie(esCookie);
+            Cookie pgCookie = new Cookie("pg","0");
+            response.addCookie(pgCookie);
+            Cookie grCookie = new Cookie("gr","0");
+            response.addCookie(grCookie);
+            Cookie kgCookie = new Cookie("kg","0");
+            response.addCookie(kgCookie);
+            Cookie totalCookie = new Cookie("total", "0");
+            response.addCookie(totalCookie);
+        } else {
+            Cookie[] valueCookie = request.getCookies();
+            for (Cookie iteration : valueCookie) {
+                if (iteration.getName().contains("name")){
+                    nameValue = iteration.getValue();
+                }
+            }
+        }
         String html = "<form method='post'>" +
-                "<input type='text' name='user' />" +
+                "<input type='text' name='user' value='" + nameValue + "'/>" +
                 "<select name='lang'>" +
                 "<option value='en' selected>English</option>" +
                 "<option value='fr'>French</option>" +
@@ -63,32 +89,66 @@ public class HelloController {
     }
     @RequestMapping(value = "greeting", method=RequestMethod.POST)
     @ResponseBody
-    public static String createMessage(HttpServletRequest request) {
+    public static String giveMessage(HttpServletResponse response, HttpServletRequest request) {
         String name = request.getParameter("user");
         String lang = request.getParameter("lang");
+        Cookie[] myCookies = request.getCookies();
         String select = "";
+        String currentLang = "";
+        String currentLangView = "";
+        String totalView = "";
         switch(lang) {
             case "en":
                 select = "Hello, ";
+                currentLang = "English";
                 break;
             case "fr":
                 select = "Bonjour, ";
+                currentLang = "French";
                 break;
             case "es":
                 select = "Hola, ";
+                currentLang = "Spanish";
                 break;
             case "pg":
                 select = "Olá, ";
+                currentLang = "Portuguese";
                 break;
             case "gr":
                 select = "Guten tag, ";
+                currentLang = "German";
                 break;
             case "kg":
                 select = "nuqneH, ";
+                currentLang = "Klingon";
                 break;
         }
+        for (Cookie iteration : myCookies) {
+            if (iteration.getName().contains(lang) || iteration.getName().contains("total")) {
+                String countStr = iteration.getValue();
+                int countInt = Integer.parseInt(countStr);
+                countInt++;
+                countStr = Integer.toString(countInt);
+                iteration.setValue(countStr);
+                if (iteration.getName().contains("total")) {
+                    totalView = iteration.getValue();
+                } else {
+                    currentLangView = iteration.getValue();
+                }
+                response.addCookie(iteration);
+            }
+        }
+
+        Cookie nameCookie = new Cookie("name", name);
+        response.addCookie(nameCookie);
+
+
         String html = "<h2>" + select + name + "!</h2>" +
-                "<img src='http://2.bp.blogspot.com/-3Lpva--N9pU/U1JJVvT7DFI/AAAAAAAAIYA/xn88y-DyKgI/s1600/Rockin+Worf.jpg' style='width: 249; height: 342;'>";
+                "<img src='http://2.bp.blogspot.com/-3Lpva--N9pU/U1JJVvT7DFI/AAAAAAAAIYA/xn88y-DyKgI/s1600/Rockin+Worf.jpg'" +
+                "style='width: 249; height: 342;'>" +
+                "<p>You viewed this page in " + currentLang + " " + currentLangView + " time(s) " +
+                "and you have seen this greeting in any language " + totalView + " time(s)." +
+                "<p><a href='/greeting'>Greet me again!</a></p>";
         return html;
     }
 
